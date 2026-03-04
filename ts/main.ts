a45ac34f89a701ed372fdf0ac2e26701ddd687a6
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-03-04 09:52:26 +0100

    Commit before btn to validate..

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,49 @@
-console.log('js-tp-drag-and-drop (w202)');
+import {settings} from "./settings";
+
+const app = {
+    addDragStartEvent(evt: DragEvent) {
+        this.liElementToMove = evt.currentTarget;
+    },
+    addDragOverEvent(evt: DragEvent) {
+        evt.preventDefault();
+    },
+    addDragEnterEvent(evt: DragEvent) {
+        (evt.currentTarget as HTMLLIElement).classList.add(settings.liDragEnterClass)
+    },
+    addDragLeaveEvent(evt: DragEvent) {
+        (evt.currentTarget as HTMLLIElement).classList.remove(settings.liDragEnterClass)
+    }, addDropEvent(evt: DragEvent) {
+        this.addDragLeaveEvent(evt);
+
+        [(evt.currentTarget as HTMLLIElement).textContent, this.liElementToMove.textContent] =
+            [this.liElementToMove.textContent, (evt.currentTarget as HTMLLIElement).textContent]
+
+    },
+    addEventListeners() {
+        // @ts-ignore
+        for (const cardElement of this.cardElements) {
+            cardElement.addEventListener('dragstart', (evt: DragEvent) => {
+                this.addDragStartEvent(evt);
+            });
+            cardElement.addEventListener('dragover', (evt: DragEvent) => {
+                this.addDragOverEvent(evt);
+            });
+            cardElement.addEventListener('dragenter', (evt: DragEvent) => {
+                this.addDragEnterEvent(evt);
+            });
+            cardElement.addEventListener('dragleave', (evt: DragEvent) => {
+                this.addDragLeaveEvent(evt);
+            });
+            cardElement.addEventListener('drop', (evt: DragEvent) => {
+                this.addDropEvent(evt);
+            });
+        }
+    },
+    init() {
+        this.cardElements = document.querySelectorAll(settings.liSelector);
+        this.liElementToMove = null;
+        this.addEventListeners();
+    },
+}
+
+app.init();
---
+++ ts/settings.ts
@@ -0,0 +1,4 @@
+export const settings = {
+    liSelector: 'li[draggable]',
+    liDragEnterClass: 'dragEnter'
+}
